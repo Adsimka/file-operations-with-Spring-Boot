7fafa75f6f5440fc662c44748154146de1af96be
Arseniy <sahalysyk02@mail.ru>
2024-06-09 15:24:28 +0500

    Implementation of functions with the possibility of operation on zip files

--- src/main/java/file_upload_download/controller/UploadDownloadWithFileSystemController.java
+++ src/main/java/file_upload_download/controller/UploadDownloadWithFileSystemController.java
@@ -2,14 +2,25 @@ package file_upload_download.controller;
 
 import file_upload_download.dto.FileUploadResponse;
 import file_upload_download.service.FileStorageService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.apache.tomcat.util.codec.binary.StringUtils;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,6 +47,34 @@ public class UploadDownloadWithFileSystemController {
                 .build();
     }
 
+    @PostMapping("multiple/upload")
+    List<FileUploadResponse> multipleUpload(@RequestParam("files") MultipartFile[] files) {
+        if (files.length > 7) {
+            throw new RuntimeException("Too many files");
+        }
+
+        List<FileUploadResponse> filesUpload = new ArrayList<>();
+        Arrays.asList(files).stream()
+                .forEach(file -> {
+                    String fileName = fileStorageService.storeFile(file);
+                    // http:/localhost:8081/download/{fileName}
+                    String url = ServletUriComponentsBuilder.fromCurrentContextPath()
+                            .path("/download/")
+                            .path(fileName)
+                            .toUriString();
+                    String contentType = file.getContentType();
+
+                    FileUploadResponse uploadResponse = FileUploadResponse.builder()
+                            .fileName(fileName)
+                            .url(url)
+                            .contentType(contentType)
+                            .build();
+
+                    filesUpload.add(uploadResponse);
+                });
+        return filesUpload;
+    }
+
     @GetMapping("/download/{fileName}")
     public ResponseEntity<Resource> downloadSingleFile(@PathVariable String fileName) {
         Resource resource = fileStorageService.download(fileName);
@@ -45,5 +84,29 @@ public class UploadDownloadWithFileSystemController {
                 .contentType(MediaType.parseMediaType(contentType))
                 .header(HttpHeaders.CONTENT_DISPOSITION, "inline;fileName=" + resource.getFilename())
                 .body(resource);
+    }
+
+    @SneakyThrows
+    @GetMapping("zipDownload")
+    public void zipDownload(@RequestParam("fileName") String[] filesName, HttpServletResponse response) {
+        try (ZipOutputStream zos = new ZipOutputStream(response.getOutputStream())) {
+            Arrays.asList(filesName).stream()
+                    .forEach(file -> {
+                        Resource resource = fileStorageService.download(file);
+                        ZipEntry entry = new ZipEntry(resource.getFilename());
+                        try {
+                            entry.setSize(resource.contentLength());
+
+                            zos.putNextEntry(entry);
+                            StreamUtils.copy(resource.getInputStream(), zos);
+                            zos.closeEntry();
+                        } catch (IOException exception) {
+                            throw new RuntimeException("Some exception while zip");
+                        }
+                    });
+            zos.finish();
+        }
+        response.setStatus(200);
+        response.addHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;fileName=zipFile");
     }
 }
